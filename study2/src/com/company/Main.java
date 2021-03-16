dab189700433282eb5f32d3be9dc944039b7045c
c0n5n <63863832+c0n5n@users.noreply.github.com>
2021-03-16 20:16:47 +0300

    Add files via upload

---
+++ study2/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//byte b = 123;
+	//short s = 240;
+	//int i = 2345436;
+	//long l = 1212;
+	//Long c = 123123142412L + 24;
+	//float f = 23425.2f;
+	//double d = 1234.56567;
+	char ch = '$'; // задаем символ по его коду
+	char ch2 = 64;
+	System.out.println((int)ch);
+	System.out.println(ch2);
+
+
+
+    }
+}
